3d76079bc236ac3208d53d54eb0ccd8c0c70e545
Tim Allen <tim@noblesamurai.com>
2020-10-05 11:58:15 +1100

    Initial port from ns-audio.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,101 @@
-export default function main (input, options = {}) {
-  if (typeof input !== 'string') {
-    throw new TypeError('Expected input to be string');
+const debug = require('debug')('ns-audio:image');
+const request = require('request');
+const path = require('path');
+const fs = require('fs');
+const ffmpeg = require('fluent-ffmpeg');
+const commandExists = require('command-exists').sync;
+const gnuplot = commandExists('gnuplot') ? 'gnuplot' : 'gnuplot-nox';
+const spawn = require('child_process').spawn;
+const tmp = require('temp');
+
+ffmpeg.setFfmpegPath(require('ffmpeg-static'));
+ffmpeg.setFfprobePath(require('ffprobe-static').path);
+
+function _getLocalFile (input) {
+  return new Promise((resolve, reject) => {
+    if (!/^https?:/.test(input)) {
+      resolve(input);
+      return;
+    }
+
+    // Download file...
+    const temporaryDir = tmp.mkdirSync('ns-audio'); // New temp dir each time incase of name conflicts
+    const filename = path.resolve(temporaryDir, path.basename(input));
+    const file = fs.createWriteStream(filename);
+    file.on('finish', () => resolve(filename));
+    file.on('error', reject);
+    request.get(input).pipe(file);
+  });
+}
+
+function _getDuration (filename) {
+  return new Promise((resolve, reject) => {
+    ffmpeg.ffprobe(filename, (err, metadata) => {
+      if (err) return reject(err);
+      const duration = metadata.streams && metadata.streams.length && Number.parseFloat(metadata.streams[0].duration);
+      if (Number.isNaN(duration)) {
+        reject(new Error('failed to parse audio track'));
+        return;
+      }
+
+      resolve(duration);
+    });
+  });
+}
+
+function plotArgs (output, width) {
+  return [
+    '-p', '-e', [
+      'set terminal png transparent truecolor size ' + width + ',100',
+      'set output "' + output + '"',
+      'unset key',
+      'unset tics',
+      'unset border',
+      'set lmargin 0',
+      'set rmargin 0',
+      'set tmargin 0',
+      'set bmargin 0',
+      "plot '<cat' binary filetype=bin format='%int16' endian=little array=1:0 lc '#000000' with lines"
+    ].join(';') + ';'
+  ];
+}
+
+module.exports = async function (input, output, done) {
+  const ffmpegOutput = tmp.path();
+  let width;
+  try {
+    const filename = await _getLocalFile(input);
+    const duration = await _getDuration(filename);
+    width = Math.round(duration * 100);
+
+    const command = ffmpeg()
+      .input(filename)
+      .withAudioCodec('pcm_s16le')
+      .withAudioChannels(1)
+      .withAudioFrequency(2000)
+      .withOutputOptions(['-map', '0:a'])
+      .withOutputFormat('data')
+      .output(ffmpegOutput)
+      .on('start', cmd => debug('ffmpeg command: %s', cmd))
+      .on('error', err => done(err))
+      .on('end', ffmpegDone);
+
+    command.run();
+  } catch (error) {
+    return done(error);
   }
 
-  return `${input} & ${options.postfix || 'rainbow'}`;
-}
+  function ffmpegDone () {
+    const inFile = fs.createReadStream(ffmpegOutput);
+    const plotProc = spawn(gnuplot, plotArgs(output, width), { stdio: ['pipe', process.stdout, process.stdout] });
+    inFile.pipe(plotProc.stdin);
+
+    plotProc.on('exit', (code, signal) => {
+      if (code !== undefined && code !== 0) {
+        return done(new Error('image exited with code: ' + code));
+      }
+
+      done();
+    });
+  }
+};
--- test/index.js
+++ test/index.js
@@ -1,3 +1,85 @@
-describe('my module', () => {
-  it('does things', () => {});
+const expect = require('expect.js');
+const image = require('..');
+const fs = require('fs');
+const path = require('path');
+const http = require('http');
+const temp = require('temp'); // .track();
+let temporaryDir;
+
+describe('image', function () {
+  this.timeout(2000);
+
+  // Create tmp directory
+  before(done => {
+    temp.mkdir('image', (err, dir) => {
+      temporaryDir = dir;
+      done(err);
+    });
+  });
+
+  it('should create a waveform image', done => {
+    const input = path.join(__dirname, 'fixtures/311.opus');
+    const output = path.join(temporaryDir, 'out-311.png');
+
+    image(input, output, err => {
+      if (err) return done(err);
+      // Make sure the output file exists
+      expect(fs.existsSync(output)).to.be(true);
+
+      // And that it has a size
+      const stats = fs.statSync(output);
+      expect(stats.size).to.be.above(2000);
+
+      done(err);
+    });
+  });
+
+  it('should error cleanly for bad input', done => {
+    const input = path.join(__dirname, 'fixtures/zero-length.opus');
+    const output = path.join(temporaryDir, 'out-zero-length.png');
+
+    image(input, output, err => {
+      expect(err).to.be.ok();
+      expect(err).to.be.an(Error);
+      done();
+    });
+  });
+
+  it('should error cleanly for bad input', done => {
+    // Bad.opus is a blank audio file that has been truncated (ie. only partially there).
+    const input = path.join(__dirname, 'fixtures/bad.opus');
+    const output = path.join(temporaryDir, 'out-bad.png');
+
+    image(input, output, err => {
+      expect(err).to.be.ok();
+      expect(err).to.be.an(Error);
+      done();
+    });
+  });
+
+  it('should create a waveform image from a remote file', done => {
+    const server = http.createServer((request, response) => {
+      if (/\.opus$/.test(request.url)) {
+        const input = fs.createReadStream(path.join(__dirname, 'fixtures', request.url));
+        input.pipe(response);
+      } else {
+        response.end();
+      }
+    }).listen(0);
+
+    const input = 'http://localhost:' + server.address().port + '/311.opus';
+    const output = path.join(temporaryDir, 'out.png');
+
+    image(input, output, err => {
+      if (err) return done(err);
+      // Make sure the output file exists
+      expect(fs.existsSync(output)).to.be(true);
+
+      // And that it has a size
+      const stats = fs.statSync(output);
+      expect(stats.size).to.be.above(2000);
+
+      done(err);
+    });
+  });
 });
